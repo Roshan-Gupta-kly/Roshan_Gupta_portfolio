09d0ab46c0e15f6b7450251e387911cbea969363
Roshan-Gupta-kly <lucysigma72@gmail.com>
2024-10-03 15:50:54 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import Technologies from "./sections/Technologies";
 import Hero from "./sections/Hero";
 import Teams from "./sections/Teams";
 import MatterAnimation from "./components/MatterAnimation";
-import SimpleBar from "simplebar-react";
-import "simplebar-react/dist/simplebar.min.css"; // Import SimpleBar CSS for styling
-import ChatIcon from "./components/ChatIcon"; // Import the chat icon
 import Services from "./sections/Services";
 import Contact from "./sections/Contact";
 import Footer from "./sections/Footer";
@@ -18,7 +15,6 @@ const App = () => {
       <MatterAnimation className="fixed top-0 left-0 h-full w-full -z-10 overflow-hidden" />
 
       <main className="relative z-10 selection:bg-slate-300 selection:text-cyan-900 w-full max-w-[100vw] overflow-x-hidden">
-        {/* Wrap sections in SimpleBar for custom scroll */}
           <Nav />
           <section className="px-4 sm:px-8 lg:px-16 z-0">
             <Hero />
@@ -38,9 +34,6 @@ const App = () => {
           <section className="">
             <Footer />
           </section>
-
-        {/* Chat Icon at the bottom-right corner */}
-        {/* <ChatIcon /> */}
       </main>
     </>
   );
